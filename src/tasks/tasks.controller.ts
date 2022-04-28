cc8a46eae95a4078c0b797b6cc7c4c3534ac0538
Shaalan Marwan <salsanjary@gmail.com>
2022-04-28 23:00:23 +0300

    use task model in controlle

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Task } from 'src/tasks/task.model';
 import { TasksService } from 'src/tasks/tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get('')
+  getTasks(): Task[] {
+    return this.tasksService.getTasks();
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from 'src/tasks/task.model';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getTasks() {
+    return this.tasks;
+  }
+}
